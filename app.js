ad6c5cf3c84e77222e0204e76bcd8ef3d20f1f12
Dylan <34342114+BoNez1337@users.noreply.github.com>
2021-05-25 10:26:50 +0100

    created anagram checker and function to log result

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const http = require('http');
+"use strict";
+var a = "steam";
+var b = "mates";
 
-const hostname = '127.0.0.1';
-const port = 3000;
+function checkWords(word1, word2) {
+    //if we split the word and then sort the letters, if they are an anagram then the sorted letters should match
+    var x = word1.split("").sort().join("");
+    var y = word2.split("").sort().join("");
+    if (x === y) {
+        //the two words match
+        return true;
+    } else {
+        //the two words do NOT match
+        return false;
+    }
+}
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+function runCheck() {
+    //if checkWords returns true then we know the words are anagrams so we should print a message informing the user that they are anagrams
+    if (checkWords(a, b)) {
+        console.log(`${a} and ${b} ARE anagrams`);
+    } else {
+        console.log(`${a} and ${b} are NOT anagrams`);
+    }
+}
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+runCheck();
+
